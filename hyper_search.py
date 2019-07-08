632b7318339ce9e1484519e87a46cc7b9cb153d5
Arsene Fansi <arsene.fansi@gmail.com>
2019-07-08 16:56:53 -0400

    Adding features for Orion based hyper-parameter search

---
+++ hyper_search.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import tensorflow as tf
+from orion.client import report_results
+
+from main import main_process
+
+
+def main(_):
+    """ This function aims at calling the main training process from which
+        it retrieves the metrics and computes the objective value to be
+        optimized by ORION. In this case, we optimize for the F1-score on
+        the validation dataset.
+    """
+
+    run_metrics = main_process()
+    default_value = 1000000.0
+    orion_objective = default_value
+    if not (run_metrics is None):
+        # val_acc = run_metrics.get('val_binary_accuracy', 0.0)
+        val_precision = run_metrics.get('val_precision', 0.0)
+        val_recall = run_metrics.get('val_recall', 0.0)
+
+        denom = val_precision + val_recall
+        if denom <= 0:
+            denom = 1e-5
+
+        val_f1 = 2 * (val_precision * val_recall) / denom
+
+        orion_objective = -val_f1  # -val_acc
+
+    report_results(
+        [dict(
+            name='orion_objective',
+            type='objective',
+            value=orion_objective)
+         ]
+    )
+
+
+if __name__ == '__main__':
+    tf.app.run(main=main)
---
+++ orion_hyper_search.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEFLT_EXP_NAME="ORION-HYPER-SEARCH-3"
+DEFLT_MAX_TRIALS=20
+
+if [ "$1" != "" ]; then
+    EXP_NAME=$1
+else
+    EXP_NAME=$DEFLT_EXP_NAME
+fi
+
+if [ "$2" != "" ]; then
+    MAX_TRIALS=$2
+else
+    MAX_TRIALS=$DEFLT_MAX_TRIALS
+fi
+
+echo $EXP_NAME
+echo $MAX_TRIALS
+
+orion -v hunt -n $EXP_NAME --max-trials $MAX_TRIALS ./hyper_search.py --mlflow_experiment_name $EXP_NAME --num_epochs 10 --num_max_chkpts 1 --output_dir /network/tmp1/fansitca/TT-Training/search_ouput --learning_rate~'loguniform(1e-5, 1.0)' --kernel_size~'choices([2, 3, 4])' --pool_size~'choices([2, 3, 4])' --num_fc~'choices([1, 2, 3])' --strides~'choices([1, 2, 3])' --dropout_rate~'uniform(0.0, 0.6)' --fc_size~'choices([256, 512, 1000, 1500, 2000])'
